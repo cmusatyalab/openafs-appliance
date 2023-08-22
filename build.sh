50ebdb6cb4fe83f4bbd8263512dcedc51ed4bdab
Jan Harkes <jaharkes@cs.cmu.edu>
2023-08-22 10:23:46 -0400

    Update base image to new debian-stable (bookworm) add winbind + realmd for AD

--- build.sh
+++ build.sh
@@ -4,7 +4,10 @@ set -ex
 
 rm -f openafs-appliance.img
 
-sudo `which vmdb2` -v --rootfs-tarball openafs-appliance.tgz \
-    --output openafs-appliance.img openafs-appliance.vmdb 
+sudo `which vmdb2` -v \
+    --rootfs-tarball openafs-appliance.tgz \
+    --log openafs-appliance.log \
+    --output openafs-appliance.img \
+    openafs-appliance.vmdb
 
 #qemu-img convert -cp -O qcow2 openafs-appliance.img openafs-appliance.qcow2
