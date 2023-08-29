ccd8441663b61b6980dc87677127f185eeec27e4
Jan Harkes <jaharkes@cs.cmu.edu>
2023-08-29 14:21:03 -0400

    Create self-signed certificate for openafs.local

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 sudo apt install -y \
     kpartx \
     ovmf \
+    openssl \
     parted \
     python3-jinja2 \
     python3-yaml \
@@ -11,3 +12,5 @@ sudo apt install -y \
     zerofree
 
 [ -e vmdb2 ] || git clone git://git.liw.fi/vmdb2
+
+[ -e openafs-appliance.crt ] || openssl req -x509 -newkey rsa:4096 -keyout openafs-appliance.key -out openafs-appliance.crt -sha256 -days 3650 -nodes -subj "/C=US/ST=Pennsylvania/L=Pittsburgh/O=CMU/OU=SCS-CSD/CN=openafs.local"
