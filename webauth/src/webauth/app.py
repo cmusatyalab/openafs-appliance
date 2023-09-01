b10373ae1d087dd65b3e1539d9454b7c0625f011
Jan Harkes <jaharkes@cs.cmu.edu>
2023-09-01 12:35:15 -0400

    Mostly working login web app.

--- webauth/src/webauth/app.py
+++ webauth/src/webauth/app.py
@@ -1,16 +1,20 @@
+import os
 import pwd
 import re
 import subprocess
 import tempfile
+from pathlib import Path
 from typing import Optional
 
 from flask import Flask, request, render_template
 
-app = Flask(__name__)
 
-USER_BLOCKLIST = (
-    "root",
-)
+KINIT = "/usr/bin/kinit"
+SMBPASSWD = "/usr/bin/smbpasswd"
+USER_BLOCKLIST = ("root",)
+
+
+app = Flask(__name__)
 
 
 def check_credentials(username: str, password: str, ccache: str) -> bool:
@@ -40,13 +44,7 @@ def check_credentials(username: str, password: str, ccache: str) -> bool:
 
     try:
         subprocess.run(
-            [
-                kinit,
-                "--password-file=STDIN",
-                "-c",
-                f"FILE:{ccache}",
-                username,
-            ],
+            [KINIT, "-c", f"FILE:{ccache}", username],
             input=f"{password}\n",
             encoding="utf-8",
             check=True,
@@ -56,32 +54,65 @@ def check_credentials(username: str, password: str, ccache: str) -> bool:
         return False
 
 
+def get_or_create_user(username: str, password: str) -> pwd.struct_passwd:
+    # check if username already exists as local user
+    try:
+        return pwd.getpwnam(username)
+    except KeyError:
+        pass
+
+    # not already a local_user, use smbpasswd to create a local user
+    try:
+        subprocess.run([SMBPASSWD, "-a", username], input=f"{password}\n", check=True)
+    except subprocess.CalledProcessError:
+        raise RuntimeError(f"Failed to add local user {username}")
+
+    try:
+        return pwd.getpwnam(username)
+    except KeyError:
+        raise RuntimeError(f"Failed to find new user {username}")
+
+
 def do_login(username: str, password: str) -> Optional[str]:
     with tempfile.NamedTemporaryFile() as cctemp:
         # check if we can authenticate this user with the kerberos realm
-        # kinit? user/password, save credentials...
         if not check_credentials(username, password, cctemp.name):
             return "Username or password incorrect"
 
-        # check if username already exists as local user
+        # make sure we have a matching local user account
         try:
-            user = pwd.getpwnam(username)
-        except KeyError:
-            # not already a local_user, use smbpasswd to create a local user
-            try:
-                subprocess.run([smbpasswd, "-a", username], input=f"{password}\n", check=True)
-                user = pwd.getpwnam(username)
-            except (subprocess.CalledProcessError, KeyError):
-                return f"Failed to add local user {username}"
-        
-        # Move saved credentials to user's krbccache
-        cc_path = Path("/tmp", f"krb5cc_{user.pw_uid}")
-        cc_temp = Path(cctemp.name)
-        cc_temp.chmod(0o600)
-        os.chown(cc_temp, user.pw_uid, user.pw_gid)
-        cc = cc_temp.replace(cc_path)
-            
+            user = get_or_create_user(username, password)
+        except RuntimeError as exc:
+            return exc.args[0]
+
+        # Move saved credentials to user's krb ccache
+        #
+        # Try to run kinit as the new user first because it probably knows best
+        # how to properly handle the credentials cache file.
+        try:
+            subprocess.run(
+                [
+                    "su",
+                    "-s",
+                    "/bin/sh",
+                    "-c",
+                    KINIT,
+                    "--login",
+                    username,
+                    username,
+                ],
+                input=f"{password}\n",
+                encoding="utf-8",
+                check=True,
+            )
+        except subprocess.CalledProcessError:
+            cc_temp = Path(cctemp.name)
+            cc_temp.chmod(0o600)
+            os.chown(cc_temp, user.pw_uid, user.pw_gid)
+            cc_temp.replace(f"/tmp/krb5cc_{user.pw_uid}")
+            Path(cctemp.name).write_bytes(b"")
     return None
+
 
 @app.route("/", methods=["GET", "POST"])
 def login():
@@ -89,9 +120,10 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        error = do_login(username, password):
+        error = do_login(username, password)
         if error is None:
-            return render_template("login_success.html", username=username)
+            # return render_template("login_success.html", username=username)
+            error = f"Successfully authenticated {username}"
     else:
         error = ""
 
